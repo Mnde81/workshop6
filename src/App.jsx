a45ff2d603d149755c754592b630c2071734161f
Mnde81 <minubis@gmail.com>
2024-08-23 19:43:47 +0300

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 
-import sectionTwoImage1 from './img/1.png';
-import sectionTwoImage2 from './img/2.png';
-import sectionTwoImage3 from './img/3.png';
-import sectionTwoImage4 from './img/4.png';
-import sectionTwoImage5 from './img/5.png';
 import SectionTwoCard from './components/SectionTwoCard'
 import SectionTwoTrend from './components/SectionTwoTrend';
 
@@ -11,21 +6,9 @@ function App() {
   
 
   return (
-    <>
-   
-
-    <div className='sectionTwoTrending'>
-      <SectionTwoTrend />
-    </div>    
-    <div className="sectionTwoCards">
-      <SectionTwoCard img={sectionTwoImage1}/>
-      <SectionTwoCard img={sectionTwoImage2}/>
-      <SectionTwoCard img={sectionTwoImage3}/>
-      <SectionTwoCard img={sectionTwoImage4}/>
-      <SectionTwoCard img={sectionTwoImage5}/>
-    </div>
-
-         
+    <>      
+      <SectionTwoTrend />    
+      <SectionTwoCard />        
     </>
   )
 }
--- src/components/SectionTwoTrend.jsx
+++ src/components/SectionTwoTrend.jsx
@@ -3,10 +3,12 @@ import style from './SectionTwoTrend.module.css';
 function SectionTwoTrend() {
     return (
         <>
-        <h2>Trending Now</h2>
-        <div className={style.trendingBoxContainer}>
-            <p className={style.trendingBox}>Lithuania <span>&#10597;</span></p>
-            <p className={style.trendingBox}>Movies <span>&#10597;</span></p>
+        <div className={style.sectionTwoTrending}>
+            <h2>Trending Now</h2>
+            <div className={style.trendingBoxContainer}>
+                 <p className={style.trendingBox}>Lithuania <span>&#10597;</span></p>
+                 <p className={style.trendingBox}>Movies <span>&#10597;</span></p>
+            </div>
         </div>
         </>
     )
